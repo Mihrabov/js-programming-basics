3f6f2f343f43586843a769296a023167de35da0e
Mihrabov <59070681+Mihrabov@users.noreply.github.com>
2019-12-26 01:39:16 +0300

    Калькулятор

--- calc.js
+++ calc.js
@@ -1,3 +1,26 @@
-// Здесь должен быть итоговый код калькулятора
+//function summ(){ 
+let number1 = Number(document.getElementById('first_number').value) 
+let number2 = Number(document.getElementById('second_number').value) 
+var answer = number1 + number2 
+document.getElementById("result").innerHTML = answer 
+} 
+function subtraction(){ 
+let number1 = Number(document.getElementById('first_number').value) 
+let number2 = Number(document.getElementById('second_number').value) 
+var answer = number1 - number2 
+document.getElementById("result").innerHTML = answer 
+} 
+function multiplication(){ 
+let number1 = Number(document.getElementById('first_number').value) 
+let number2 = Number(document.getElementById('second_number').value) 
+var answer = number1 * number2 
+document.getElementById("result").innerHTML = answer 
+} 
+function division(){ 
+let number1 = Number(document.getElementById('first_number').value) 
+let number2 = Number(document.getElementById('second_number').value) 
+var answer = number1 / number2 
+document.getElementById("result").innerHTML = answer 
+}// Здесь должен быть итоговый код калькулятора
 
-console.log('Скрипт загружен');
+console.log('Скрипт загружен'); Здесь должен быть итоговый код калькулятора
